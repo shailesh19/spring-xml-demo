8d981460ef60d44f737cbac69831e60c4d1bca0b
shailesh <beingshailesh19@gmail.com>
2019-07-28 11:07:03 +0530

    Task 1 in Spring fundamentals uploaded.

---
+++ src/main/java/com/stackroute/domain/Main.java
@@ -0,0 +1,25 @@
+package com.stackroute.domain;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Actor actor = context.getBean("actor",Actor.class);
+        System.out.println(actor);
+
+        XmlBeanFactory xmlBeanFactory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
+        Actor actor1 = xmlBeanFactory.getBean("actor",Actor.class);
+        System.out.println(actor1);
+
+        Actor actor2 = xmlBeanFactory.getBean("actor2",Actor.class);
+        System.out.println(actor2);
+
+    }
+}
